005e3dbf958abedf3c515cadb17df8df086c8bc2
Myantel <myantel@Myantels-Mac-mini.local>
2015-05-26 16:38:30 +0630

    Story Board Done

---
+++ Betting/SoccerTableViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  SoccerTableViewCell.swift
+//  Betting
+//
+//  Created by Myantel on 5/26/15.
+//  Copyright (c) 2015 Myantel. All rights reserved.
+//
+
+import UIKit
+
+class SoccerTableViewCell: UITableViewCell {
+
+    //Score Label
+    @IBOutlet weak var homeTeamScore: UILabel!
+    @IBOutlet weak var visitTeamScore: UILabel!
+    
+    //Team Name Label
+    @IBOutlet weak var homeTeamName: UILabel!
+    @IBOutlet weak var visitTeamName: UILabel!
+    
+    //Bet Rate
+    @IBOutlet weak var homeTeamRate: UILabel!
+    @IBOutlet weak var visitTeamRate: UILabel!
+    
+    //Status Label
+    @IBOutlet weak var liveLabel: UILabel!
+    @IBOutlet weak var minuteLabel: UILabel!
+    
+    
+    //Background Image
+    @IBOutlet weak var backImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
